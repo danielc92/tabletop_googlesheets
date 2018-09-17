4f94d562b43cba9322b04c29f3eac12a7db026bc
daniel corcoran <danielc.92g@gmail.com>
2018-09-17 12:09:32 +1000

    Added working basic example

---
+++ import.js
@@ -0,0 +1,25 @@
+  window.onload = function() {
+    init()
+  };
+
+  var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1XEzWBDSDvxPBOZ5Fgh6JnaGFiz92y5rIj3GkbTvP--U/pubhtml';
+
+  function init() {
+    Tabletop.init({
+      key: public_spreadsheet_url,
+      callback: showInfo,
+      simpleSheet: true
+    })
+  }
+
+  function showInfo(data, tabletop) {
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+      tr = $('<tr/>');
+      tr.append("<td>" + data[i].name_ + "</td>");
+      tr.append("<td>" + data[i].age_ + "</td>");
+      tr.append("<td>" + data[i].score_ + "</td>");
+      $('#table1').append(tr);
+    }
+    console.log(data);
+  }
